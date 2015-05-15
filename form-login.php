5e1968e19e513cf4f17db4a101848f351906a016
Alex Miranda <amdesignercontato@gmail.com>
2015-05-14 23:09:36 -0300

    Criando form e css da estrutura inicial

---
+++ form-login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Form login</title>
+	<link rel="stylesheet" href="assets/css/style-form.css" />
+</head>
+<body>
+
+	<div class="container">
+		<form action="" class="form form-login">
+			<input type="email" placeholder="Email" requered />
+			<input type="password" placeholder="Senha" requered />
+			<input type="submit" value="Enviar" />
+			<div class="remember-and-links">
+				<input type="checkbox" name="remember" />
+			</div>
+		</form>
+	</div>
+
+</body>
+</html>
