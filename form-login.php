97959a26d8c9d8ef771151dc19de7240f51fbcfd
Alex Miranda <amdesignercontato@gmail.com>
2015-05-19 21:31:07 -0300

    Inserindo a tag viewport para responsividade, alterando lang para pr-br

--- form-login.php
+++ form-login.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
-	<meta charset="UTF-8">
 	<title>Form login</title>
+
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">	
+	
 	<link rel="stylesheet" href="assets/css/style-form.css" />
 </head>
 <body>
